8c27935ef61cfb0ee79b4a9be4f8d14acec7f5c5
Kilian Gosewisch <kilian2798@gmail.com>
2022-10-16 22:48:15 +0200

    update shop link

--- consts.ts
+++ consts.ts
@@ -1,5 +1,4 @@
-export const SHOP_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSdqmBgAEWvLBqIgPvQOdDDWBg5mtdyXknu0w3g9fkRDjgDw6Q/viewform";
+export const SHOP_URL = "https://www.ebay.com/sch/freeyourstream/m.html";
 export const DISCORD_URL = "https://discord.gg/sEt2Rrd";
 export const GITHUB_URL = "https://github.com/freeyourstream";
 export const CONFIGURATOR_URL = "https://fdconfig.freeyourstream.com";
